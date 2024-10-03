5ddfea7122bb2d23fa2e426339eb624b8a2e1fe6
linporu <qazwsx123456guy@gmail.com>
2024-10-03 18:50:43 +0800

    Upload week 0 problem set "players"

---
+++ Week 0 Querying/Players/10.sql
@@ -0,0 +1,4 @@
+SELECT "birth_city" AS 'City'
+FROM "players"
+WHERE "birth_year" > 2000
+ORDER BY "birth_city"
---
+++ Week 0 Querying/Players/7.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*)
+FROM "players"
+WHERE ("bats" = 'R' AND "throws" = 'L')
+    OR ("bats" = 'L' AND "throws" = 'R');
+
