58a5ce8bab5619500c59dafb66645e302267724a
linporu <qazwsx123456guy@gmail.com>
2024-10-06 21:48:37 +0800

    refactor: use INTERSECT in pset "Moneyball" 12.sql

--- Week 1 Relating/MoneyBall/12.sql
+++ Week 1 Relating/MoneyBall/12.sql
@@ -1,7 +1,6 @@
 SELECT "first_name", "last_name"
-FROM "players"
-WHERE "players"."id" IN (
-    SELECT "players"."id"
+FROM (
+    SELECT "players"."id", "first_name", "last_name"
     FROM "players"
     JOIN "performances" ON "players"."id" = "performances"."player_id"
     JOIN "salaries" ON "players"."id" = "salaries"."player_id"
@@ -12,16 +11,18 @@ WHERE "players"."id" IN (
     ORDER BY ("salary" / SUM("H")) ASC
     LIMIT 10
 )
-    AND "players"."id" IN (
-        SELECT "players"."id"
-        FROM "players"
-        JOIN "performances" ON "players"."id" = "performances"."player_id"
-        JOIN "salaries" ON "players"."id" = "salaries"."player_id"
-        WHERE "performances"."year" = '2001'
-            AND "salaries"."year" = '2001'
-        GROUP BY "players"."id"
-        HAVING SUM("RBI") > 0
-        ORDER BY ("salary" / SUM("RBI")) ASC
-        LIMIT 10
-    )
-ORDER BY "players"."id" ASC;
+INTERSECT
+SELECT "first_name", "last_name"
+FROM (
+    SELECT "players"."id", "first_name", "last_name"
+    FROM "players"
+    JOIN "performances" ON "players"."id" = "performances"."player_id"
+    JOIN "salaries" ON "players"."id" = "salaries"."player_id"
+    WHERE "performances"."year" = '2001'
+        AND "salaries"."year" = '2001'
+    GROUP BY "players"."id"
+    HAVING SUM("RBI") > 0
+    ORDER BY ("salary" / SUM("RBI")) ASC
+    LIMIT 10
+)
+ORDER BY "last_name", "first_name";
